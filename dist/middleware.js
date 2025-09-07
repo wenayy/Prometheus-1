8eac11de0f988db0e7d4bc3aed5803ce698180a4
vinay joshi <vinay@vinay-2.local>
2025-09-07 05:47:09 +0530

    Remove dist folder from Git tracking

--- dist/middleware.js
+++
@@ -1,7 +0,0 @@
-export function middleware(req, res, next) {
-    const t = Date.now();
-    next();
-    const k = Date.now();
-    console.log((((k - t) / 1000).toFixed(3)));
-}
-//# sourceMappingURL=middleware.js.map
